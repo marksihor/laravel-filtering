479552ef236bc994478abd7d28333c961efde12f
Your Name <you@example.com>
2020-05-24 12:26:51 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # laravel-filtering
+
+The package allows you to make model filter by user request in 1 line, you just need to use the MarksIhor\LaravelFiltering\Filterable trait in your Controller.php (or whatever your controller you need the filter to work).
+
+
+## Usage
+
+### Use trait on Controller
+
+```php
+<?php
+
+namespace App\Http\Controllers;
+
+use MarksIhor\LaravelMessaging\Filterable;
+
+class User extends Authenticatable
+{
+    use Filterable;
+}
+```
+
+### Usage examples (in Controller)
+
+```php
+
+$collection = $this->filter(Model::query())->get(); // give user ability to filter model without any constraints
+$collection = $this->filter(Model::where('price', 100))->get(); // predefined filter that user cannot override in request parameters
+$collection = $this->rawParams(['status' => 'paid'])->filter(Model::query())->get(); // also a way to predefine parameters that user cannot override
+$collection = $this->filterable(['status','paid'])->filter(Model::query())->get(); // allows you to define columns that user can filter
+
+```
+
+### Usage examples (in Model)
+
+```php
+
+public $filterable = []; // to define columns that use wil be able to filter
+
+```
+
+### Usage examples (in url)
+
+```http request
+https://yoursite.com/path?status=active
+https://yoursite.com/path?price=100,75
+https://yoursite.com/path?price=100,75&status=paid
+https://yoursite.com/path?price[from]=100&price[to]=500
+https://yoursite.com/path?price[orderBy]=asc
+```
+
+As you can see you can combine parameters in the request (order of parameters is doesn't matter).
+
+## License
+
+MIT
---
+++ src/Filterable.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace MarksIhor\LaravelFiltering;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
+
+trait Filterable
+{
+    private $rawParams = [];
+    private $filterable = null;
+
+    public function filterable(array $columns)
+    {
+        $this->filterable = $columns;
+
+        return $this;
+    }
+
+    public function filter($object)
+    {
+        $builder = $object instanceof Builder ? $object : $object->getQuery();
+        $model = $builder->getModel();
+        $filterableColumns = $this->filterable ?? $model::$filterable ?? null;
+
+        $queryParams = array_merge(request()->all(), $this->rawParams);
+
+        foreach ($queryParams as $key => $value) {
+            if (
+                $this->isColumnExist($model, $key) &&
+                (
+                    !$filterableColumns ||
+                    key_exists($key, $this->rawParams) ||
+                    in_array($key, $filterableColumns)
+                )
+            ) {
+                if (is_array($value)) $this->arrayParamsFilter($builder, $key, $value);
+                else $this->keyValueFilter($builder, $key, $value);
+            }
+        }
+
+        return $object;
+    }
+
+    public function rawParams(array $params)
+    {
+        $this->rawParams = $params;
+
+        return $this;
+    }
+
+    private function isColumnExist(Model $model, string $column): bool
+    {
+        return Schema::hasColumn($model->getTable(), $column);
+    }
+
+    private function arrayParamsFilter(Builder $builder, string $key, array $value): void
+    {
+        if (isset($value['from'], $value['to'])) {
+            $builder->whereBetween($key, [$value['from'], $value['to']]);
+        } elseif (isset($value['from'])) {
+            $builder->where($key, '>=', $value['from']);
+        } elseif (isset($value['to'])) {
+            $builder->where($key, '<=', $value['to']);
+        } elseif (isset($value['orderBy']) && in_array($value['orderBy'], ['asc', 'desc'])) {
+            if ($value['orderBy'] == 'desc') $builder->orderBy($key, $value['orderBy']);
+            else $builder->orderBy($key);
+        }
+    }
+
+    private function keyValueFilter(Builder $builder, string $key, string $value): void
+    {
+        if (in_array($value, ['null', 'today', 'past', 'future'])) {
+            if ($value === 'null') $builder->where($key, null);
+            elseif ($value === 'today') $builder->whereDate($key, date('Y-m-d'));
+            elseif ($value === 'future') $this->arrayParamsFilter($builder, $key, ['from' => date('Y-m-d')]);
+            elseif ($value === 'past') $this->arrayParamsFilter($builder, $key, ['to' => date('Y-m-d')]);
+        } else {
+            $values = explode(',', $value);
+
+            if (count($values) === 1) {
+                if (is_numeric($value)) {
+                    $builder->where($key, $value);
+                } else {
+                    $builder->where($key, 'like', "%$value%");
+                }
+            } else {
+                $builder->whereIn($key, $values);
+            }
+        }
+    }
+}
